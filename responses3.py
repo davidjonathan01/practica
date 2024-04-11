a1cfbdc4e583c087a739d4295851afb5a0d3e9aa
davidjonathan <davidfernandezdavila01@gmail.com>
2024-04-10 22:32:49 -0500

    Primer commit

---
+++ responses3.py
@@ -0,0 +1,11 @@
+from flask import redirect
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+ return redirect('http://www.example.com')
+
+if __name__ == '__main__':
+ app.run(debug=True)
